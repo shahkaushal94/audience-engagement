b93cbcc2a988af19304031d523d81e4cfaa1a0e3
Thomas Munduchira <thomasjojimunduchira@gmail.com>
2017-03-04 10:11:16 -0800

    Make graph scrollable

--- public/js/index.js
+++ public/js/index.js
@@ -2,17 +2,17 @@ $(document).ready(function() {
   var chart = new Chart($('#chart'), {
     type: 'line',
     data: {
-      labels: ["", "", "", "", ""],
+      labels: ["", "", "", "", "", "", "", ""],
       datasets: [{
         label: 'Personal Tone',
         fill: false,
-        data: [12, 19, 3, 5, 2, 3],
+        data: [12, 19, 3, 5, 2, 3, 10, 3, 4],
         borderColor: '#f39c12',
         borderWidth: 1
       }, {
         label: 'Audience Engagement',
         fill: false,
-        data: [10, 14, 3, 15, 2, 13],
+        data: [10, 14, 3, 15, 2, 13, 1, 31, 1],
         borderColor: '#2980b9',
         borderWidth: 1
       }]
