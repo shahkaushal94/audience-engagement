b60ebfd99ea183fa5723387cdfa1bc17cb1018fa
Thomas Munduchira <thomasjojimunduchira@gmail.com>
2017-03-04 15:01:03 -0800

    Add splash page and transitions

--- public/js/index.js
+++ public/js/index.js
@@ -7,14 +7,14 @@ $(document).ready(function() {
       datasets: [{
         label: 'Personal Tone',
         fill: false,
-        data: [12, 19, 3, 5, 2, 3, 10, 3, 4],
+        data: [20, 91, -30, 52, -21, 35, 80, 34, -64],
         backgroundColor: 'yellow',
         borderColor: 'yellow',
         borderWidth: 1
       }, {
         label: 'Audience Engagement',
         fill: false,
-        data: [10, 14, 3, 15, 2, 13, 1, 31, 1],
+        data: [90, 41, 35, 21, -60, -85, 21, 56, -12],
         backgroundColor: 'white',
         borderColor: 'white',
         borderWidth: 1
@@ -34,7 +34,8 @@ $(document).ready(function() {
             display:false
           },
           ticks: {
-            beginAtZero: true
+            min: -100,
+            max: 100
           }
         }]
       },
@@ -43,4 +44,53 @@ $(document).ready(function() {
       }
     }
   });
+
+  $('#start-splash-page').on("click", function() {
+    controllers.start();
+  });
+  $('#stop').on("click", function() {
+    controllers.stop();
+  });
+  $('#start-recording-page').on("click", function() {
+    controllers.continue();
+  });
 });
+
+var timer;
+
+var controllers = {
+  start: function() {
+    view.changeView();
+    this.continue();
+  },
+  continue: function() {
+    $('#start-recording-page').css('display', 'none');
+    $('#stop').css('display', 'block');
+    _this = this;
+    timer = setInterval(function() {
+      _this.update() 
+    }, 5000);
+  },
+  stop: function() {
+    $('#stop').css('display', 'none');
+    $('#start-recording-page').css('display', 'block');
+    clearInterval(timer);
+  },
+  update: function() {
+    $.get({
+      url: "emotions.py"
+    }).done(function(data) {
+
+    }).fail(function(err) {
+      console.log('Failed:', err);
+    });
+  }
+};
+
+var view = {
+  changeView: function() {
+    $('#splash-page').css('display', 'none');
+    $('#recording-page').css('display', 'block');
+  }
+}
+
