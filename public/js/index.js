ffa4cf3847df7e64ddbd321c092c4e31c6e20c8e
Thomas Munduchira <thomasjojimunduchira@gmail.com>
2017-03-04 06:52:00 -0800

    Add basic line graph

---
+++ public/js/index.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+  var chart = new Chart($('#chart'), {
+    type: 'line',
+    data: {
+      labels: ["", "", "", "", ""],
+      datasets: [{
+        label: 'Personal Tone',
+        fill: false,
+        data: [12, 19, 3, 5, 2, 3],
+        borderColor: '#f39c12',
+        borderWidth: 1
+      }, {
+        label: 'Audience Engagement',
+        fill: false,
+        data: [10, 14, 3, 15, 2, 13],
+        borderColor: '#2980b9',
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        xAxes: [{
+          gridLines: {
+            display:false
+          }
+        }],
+        yAxes: [{
+          gridLines: {
+            display:false
+          },
+          ticks: {
+            beginAtZero:true
+          }
+        }]
+      }
+    }
+  });
+});
